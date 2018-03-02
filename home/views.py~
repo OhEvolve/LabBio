fbe23c149463abf0ae67598408f4e7d73bc34f31
pholec <pholec@housebirnbaum.mit.edu>
2018-03-02 17:57:52 -0500

    models for reagents app

--- home/views.py~
+++ home/views.py~
@@ -1,16 +1,16 @@
 
 from django.views import generic
 
-from reagents.models import Reagent
+from reagents.models import Liquid,Solid,Biologic,Solution
 
 class OverView(generic.ListView):
     template_name = 'home/home.html'
     context_object_name = 'overview'
 
     def get_queryset(self):
-        return Reagent.objects.order_by('pub_date')
+        return Liquid.objects.order_by('name')
 
     def get_context_data(self, **kwargs):
         context = super(OverView, self).get_context_data(**kwargs)
-        context['reagent_count'] = Reagent.objects.count()
+        context['reagent_count'] = sum([obj.objects.count() for obj in (Liquid,Solid,Biologic,Solution)])
         return context
--- reagents/models.py
+++ reagents/models.py
@@ -1,12 +1,63 @@
+
+'''
+Models for the reagents app
+'''
+
+# library imports
 from django.db import models
 
-# Create your models here.
 
-class Reagent(models.Model):
+SEQUENCE_FORM_CHOICES = (("p",     "plasmid"),
+                         ("l",     "genomic"),
+                         ("r", "recombinant"))
+                         
+SEQUENCE_SHAPE_CHOICES = (("c",   "circular"),
+                          ("l",     "linear"))
 
-    name = models.CharField(max_length=100)
-    mw = models.CharField(max_length=20)
-    pub_date = models.DateTimeField('date published')
+SEQUENCE_MATERIAL_CHOICES = (("dD",       "dsDNA"),
+                             ("sD",       "ssDNA"),
+                             ("dR",       "dsRNA"),
+                             ("sR",       "ssRNA"),
+                             ("pp",     "protein"))                          
 
-    def __str__(self):
-        return self.name
+class Matter(models.Model):
+	""" Matter reference object (base class) """
+	name = models.CharField(max_length=255)
+	owner = models.CharField(max_length=255)
+    
+	def __str__(self):
+		return '{} - {}'.format(type(self).__name__,self.name)
+
+class Liquid(Matter):
+	
+	""" Liquid reference object """
+	
+	pass
+	
+	
+class Solid(Matter):
+	
+	""" Solid reference object """
+	
+	mw = models.CharField(max_length=20)
+	
+	
+class Biologic(Matter):
+   	
+   """ Sequence codon"""
+   
+   sequence = models.CharField(max_length=255)
+   form = models.CharField(max_length = 1,choices = SEQUENCE_FORM_CHOICES)
+   shape = models.CharField(max_length = 1,choices = SEQUENCE_SHAPE_CHOICES)
+   material = models.CharField(max_length = 2,choices = SEQUENCE_MATERIAL_CHOICES)
+                  		   
+                  		   
+                  	   
+class Solution(Matter):
+	
+	""" Liquid reference object """
+	
+	pass
+                  		   
+                  		   
+            
