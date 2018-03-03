bf59a24ca271040e4edf2d19e0130f8116e49788
Patrick Holec <pholec@mit.edu>
2018-03-03 18:43:01 -0500

    tables for reagents

--- home/views.py
+++ home/views.py
@@ -13,4 +13,8 @@ class OverView(generic.ListView):
     def get_context_data(self, **kwargs):
         context = super(OverView, self).get_context_data(**kwargs)
         context['reagent_count'] = sum([obj.objects.count() for obj in (Liquid,Solid,Biologic,Solution)])
+        context['liquids'] = Liquid.objects.all()
+        context['solids'] = Solid.objects.all()
+        context['biologics'] = Biologic.objects.all()
+        context['solutions'] = Solution.objects.all()
         return context
